6ae35aeaf49a24892ee606969a8885c7230538a5
Ana <64834894+anacalo24@users.noreply.github.com>
2021-05-22 16:59:59 +0200

    Add files via upload

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -91,7 +91,8 @@ int main(void)
   MX_TIM4_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
   /* USER CODE END 2 */
 
   /* Infinite loop */
