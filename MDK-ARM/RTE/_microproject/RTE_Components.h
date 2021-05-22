0f81084c09d4e4bf44aa67b722e93096b18cd7f2
Ana <64834894+anacalo24@users.noreply.github.com>
2021-05-22 17:07:10 +0200

    Add files via upload

--- MDK-ARM/RTE/_microproject/RTE_Components.h
+++ MDK-ARM/RTE/_microproject/RTE_Components.h
@@ -1,42 +1,21 @@
-
-
-/*
-
- * Auto generated Run-Time-Environment Configuration File
-
- *      *** Do not modify ! ***
-
- *
-
- * Project: 'microproject' 
-
- * Target:  'microproject' 
-
- */
-
-
-
-#ifndef RTE_COMPONENTS_H
-
-#define RTE_COMPONENTS_H
-
-
-
-
-
-/*
-
- * Define the Device Header File: 
-
- */
-
-#define CMSIS_device_header "stm32f4xx.h"
-
-
-
-
-
-
-
-#endif /* RTE_COMPONENTS_H */
-
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'microproject' 
+ * Target:  'microproject' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/microproject/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+microproject\startup_stm32f407xx.o: startup_stm32f407xx.s
