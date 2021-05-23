2fec8d9c146d0c6aa9be17dea7f4e0b8ba368f73
Ana <64834894+anacalo24@users.noreply.github.com>
2021-05-23 10:34:18 +0200

    Add files via upload

---
+++ Core/Src/led.c
@@ -0,0 +1,37 @@
+/**
+* @file led.c
+* @Author Alejandro López Rodríguez and Ana Maria Casanova López
+* @date 16/05/2021
+* @brief Led handler
+*/
+
+/**
+* @brief Initialize led hardware
+* @param void
+* @return void
+*/
+void Led_H_Init(void){}
+
+//LED on
+/**
+* @brief Led On
+* @param LED_GPIO
+* @return void
+*/
+void Led_on(LED_GPIO){}
+
+//LED off
+/**
+* @brief Led Off
+* @param LED_GPIO
+* @return void
+*/
+void Led_off(LED_GPIO){}
+
+//Toggle LED with spec on and off time
+/**
+* @brief Led toggle
+* @param int LED_GPIO, float t_on, float t_off
+* @return void
+*/
+void Led_off(int LED_GPIO, float t_on, float t_off){}
---
+++ Core/Src/lift.c
@@ -0,0 +1,33 @@
+/**
+* @file lift.c
+* @Author Alejandro López Rodríguez and Ana Maria Casanova López
+* @date 16/05/2021
+* @brief Lift implementation
+*/
+
+/**
+* @brief Initialize the lift
+* @param void
+* @return void
+*/
+void lift_Init(void){}
+/**
+* @brief Control the lift to go up
+* @return void
+*/
+void liftUp(void){}
+/**
+* @brief Control the lift to go down
+* @return void
+*/
+void liftDown(void){}
+/**
+* @brief Retrieve wether the lift is moving or stopped
+* @return 1 if it is moving, 0 if it is stopped
+*/
+uint32_t liftIsMoving(void){}
+/**
+* @brief Retrieve the floor where the lift is
+* @return 0 ground floor, 1 first floor, -1 moving
+*/
+int32_t liftFloor(void){}
