4af2614c582663508c41b78b0af515871c1246bc
Ana <64834894+anacalo24@users.noreply.github.com>
2021-06-07 03:46:13 +0200

    Update Servo_PWM_Angle.c

--- LaTeX project/Slides/Code/Servo_PWM_Angle.c
+++ LaTeX project/Slides/Code/Servo_PWM_Angle.c
@@ -2,7 +2,6 @@ void Servo_PWM_Angle(uint16_t Servo, uint8_t angle){
 	uint32_t Pulse = 0;
 	/* Calculate the Pulse value given a duty cycle percentage */
 	if(angle > 180) Pulse = 224;
-	else if(angle == 0) Pulse = 74;
 	else Pulse = (uint32_t)(angle * (5.0/6.0) + 74);
 	/* Update the PWM duty cycle by modifying the CCRx value */
 	if(Servo == Servo1_Pin) /* Orange LED */
